416b4f9986d01cd705d83003c7f4e2e6052c30be
Aryan Poonacha <poonacharyan@gmail.com>
2023-09-20 01:51:10 +0000

    added pdf report

--- describe_stats.py
+++ describe_stats.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+from fpdf import FPDF
 
+#provides summary stats in a pdf file and graph, both in the output folder
 def describe(filepath):
+    
     df = pd.read_csv(filepath)
     print(df.describe())
+    # Generate the summary report
+    summary_report = df.describe()
+    
+    # Format the summary report as a table
+    summary_table = tabulate(summary_report, headers="keys")
+    
+    # Save the summary report to a PDF file
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.multi_cell(0, 10, txt=summary_table, align="C")
+    pdf.output('output/summary_report.pdf')
 
     # Create the scatter plot
     plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
